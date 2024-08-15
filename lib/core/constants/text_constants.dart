c84737f5a0d076f59d28f2436c1488fd33f3d1d1
mahmoodhamdi <hmdy7486@gmail.com>
2024-08-15 13:55:48 +0300

    feat(blog): Add blog creation page with image picker and topic selection
    - Implemented `AddBlogPage` with a form for adding a blog post.
    - Integrated image picker functionality for blog cover images.
    - Added topic selection using chips for better categorization of blog posts.
    - Created utility function `pickImage` for selecting images from the gallery.
    - Added `TextConstants` for predefined topics and error messages.
    - Updated `BlogPage` with a navigation to the `AddBlogPage`.
    - Added `BlogEditor` widget for better text input handling.

---
+++ lib/core/constants/text_constants.dart
@@ -0,0 +1,22 @@
+class TextConstants {
+  static const List<String> topics = [
+    'Technology',
+    'Business',
+    'Gaming',
+    'Books',
+    'Programming',
+    'Entertainment',
+    'Sports',
+    'Health',
+    'Science',
+    'World',
+    'Media',
+    'Fashion',
+    'Travel',
+    'Music',
+    'Food',
+    'Nature',
+  ];
+
+  static const noConnectionErrorMessage = 'Not connected to a network!';
+}
